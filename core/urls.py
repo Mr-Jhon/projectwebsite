564e014b4112680eea75151385665328e6203e01
Mr-Jhon <siddiqkm621@gmail.com>
2026-08-09 15:41:32 +0700

    Add article list and detail pages

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
     path('tentang/', views.tentang, name='tentang'),
+
     path('kontak/', views.kontak, name='kontak'),
+
+    path('artikel/', views.daftar_artikel, name='daftar_artikel'),
+
+    path(
+        'artikel/<int:id>/',
+        views.detail_artikel,
+        name='detail_artikel'
+    ),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Artikel
 
 
@@ -18,3 +18,23 @@ def tentang(request):
 
 def kontak(request):
     return render(request, 'core/kontak.html')
+
+
+def daftar_artikel(request):
+    artikel = Artikel.objects.all().order_by('-tanggal_dibuat')
+
+    return render(
+        request,
+        'core/daftar_artikel.html',
+        {'artikel': artikel}
+    )
+
+
+def detail_artikel(request, id):
+    artikel = get_object_or_404(Artikel, id=id)
+
+    return render(
+        request,
+        'core/detail_artikel.html',
+        {'artikel': artikel}
+    )
